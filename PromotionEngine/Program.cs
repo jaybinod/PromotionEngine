1091e52ebe0bc009ba79b8cb6d30a2a9d285c690
jaybi <jaybi@DESKTOP-JH214M4>
2020-09-16 09:51:57 +0530

    Add project files.

---
+++ PromotionEngine/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create list of promotions
+            //we need to add information about Product's count
+            Dictionary<String, int> offer1 = new Dictionary<String, int>();
+            offer1.Add("A", 3);
+            Dictionary<String, int> offer2 = new Dictionary<String, int>();
+            offer2.Add("B", 2);
+            Dictionary<String, int> offer3= new Dictionary<String, int>();
+            offer3.Add("C", 1);
+            offer3.Add("D", 1);
+
+            //Adding here product offer promotion rate and discount value
+            List<Promotion> promotions = new List<Promotion>()
+            {
+                new Promotion(1, offer1, 130, 20),
+                new Promotion(2, offer2, 45, 15),
+                new Promotion(3, offer3, 30, 5)
+            };
+
+            //create orders
+            List<Order> orders = new List<Order>();
+            Order order1 = new Order(1, new List<Product>() { new Product("A"), new Product("B"), new Product("C") });
+            Order order2 = new Order(2, new List<Product>() { new Product("A"), new Product("A"), new Product("A"), new Product("A"), new Product("A"), new Product("B"), new Product("B"), new Product("B"), new Product("B"), new Product("B"), new Product("C") });
+            Order order3 = new Order(3, new List<Product>() { new Product("A"), new Product("A"), new Product("A"), new Product("B"), new Product("B"), new Product("B"), new Product("B"), new Product("B"), new Product("C"), new Product("D") });
+            orders.AddRange(new Order[] {order1, order2, order3 });
+            //check if order meets promotion
+            foreach (Order ord in orders)
+            {
+                List<decimal> promoPrices = promotions
+                    .Select(promo => PromotionChecker.GetTotalPriceDiscount(ord, promo))
+                    .ToList();
+                decimal origPrice = ord.Products.Sum(prod => prod.Price);
+                decimal promoPriceDiscount = promoPrices.Sum();
+                Console.WriteLine($"OrderID: {ord.OrderID} => Original price: {origPrice.ToString("0.00")} | Promotional Discount: {promoPriceDiscount.ToString("0.00")} | Final price: {(origPrice - promoPriceDiscount).ToString("0.00")}");
+            }
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+        }
+
+        
+    }
+}
+
---
+++ PromotionEngine/promotionEngineClass.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine
+{
+
+    public class Product
+    {
+        public string Id { get; set; }
+        public decimal Price { get; set; }
+
+        public Product(string id)
+        {
+            this.Id = id;
+            switch (id)
+            {
+                case "A":
+                    this.Price = 50m;
+
+                    break;
+                case "B":
+                    this.Price = 30m;
+
+                    break;
+                case "C":
+                    this.Price = 20m;
+
+                    break;
+                case "D":
+                    this.Price = 15m;
+                    break;
+            }
+        }
+    }
+
+    public class Promotion
+    {
+        public int PromotionID { get; set; }
+        public Dictionary<string, int> ProductInfo { get; set; }
+        public decimal PromoPrice { get; set; }
+        public decimal PromoPriceDiscount { get; set; }
+        public Promotion(int _promID, Dictionary<string, int> _prodInfo, decimal _pp, decimal ppd)
+        {
+            this.PromotionID = _promID;
+            this.ProductInfo = _prodInfo;
+            this.PromoPrice = _pp;
+            this.PromoPriceDiscount = ppd;
+        }
+    }
+
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public List<Product> Products { get; set; }
+
+        public Order(int _oid, List<Product> _prods)
+        {
+            this.OrderID = _oid;
+            this.Products = _prods;
+        }
+    }
+
+    public static class PromotionChecker
+    {
+        //get count of promoted products from promotion and return discount value
+        public static decimal GetTotalPriceDiscount(Order ord, Promotion prom)
+        {
+            decimal d = 0M;
+            var copp = ord.Products
+                .GroupBy(x => x.Id)
+                .Where(grp => prom.ProductInfo.Any(y => grp.Key == y.Key && grp.Count() >= y.Value))
+                .Select(grp => grp.Count())
+                .Sum();
+            
+            int ppc = prom.ProductInfo.Sum(kvp => kvp.Value);
+            while (copp >= ppc)
+            {
+                d += prom.PromoPriceDiscount;
+                copp -= ppc;
+            }
+            return d;
+        }
+    }
+}
